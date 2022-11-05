506e78ef9faf8fd5c2d0ae57e608fb6a360aea1d
Nazlı Sıla Özakca <77808238+nazlisilaozakca@users.noreply.github.com>
2022-11-05 04:13:31 +0300

    Add Back End Fully

---
+++ Location.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreLocationApp
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
---
+++ Response.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreLocationApp
+{
+    public class Response
+    {
+        public object Value { get; set; }
+        public string Result { get; set; }
+        public bool Status { get; set; }
+    }
+}
